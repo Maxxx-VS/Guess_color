7e40c7861c60182f324998eb0d0a06f37ee8174f
Высочанский Максим <126272688+maxxx-vs@users.noreply.github.com>
2023-12-10 18:39:21 +0600

    Игра "Угадай цвет квадрата" №7 внешний рандом, все кнопки работают

--- 111.py
+++ 111.py
@@ -1,18 +1,22 @@
-# from tkinter import *
-import random
-from PIL import ImageColor
-rgb = t = (0, 0, 0)
+# # from tkinter import *
+# import random
+# from PIL import ImageColor
+# rgb = t = (0, 0, 0)
+#
+#
+# ws = Tk()
+# ws.title('PythonGuides')
+# ws.geometry('400x300')
+# ws.config(bg='#%0x%0x%0x' % (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256)))
+# rgb = ImageColor.getcolor(ws.cget('bg'), "RGB")
+# label = Label(ws, text=f"{rgb}", bg='cadet blue', font='Arial 30').pack()
+# print(rgb)
+# ws.mainloop()
+#
+#
+# config=print(ImageColor.getcolor(root.cget('bg'), "RGB"))
 
-
-ws = Tk()
-ws.title('PythonGuides')
-ws.geometry('400x300')
-ws.config(bg='#%0x%0x%0x' % (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256)))
-rgb = ImageColor.getcolor(ws.cget('bg'), "RGB")
-label = Label(ws, text=f"{rgb}", bg='cadet blue', font='Arial 30').pack()
-print(rgb)
-ws.mainloop()
-
-
-config=print(ImageColor.getcolor(root.cget('bg'), "RGB"))
-
+h = ['#75EBF0']
+h1 = "".join(h).lstrip('#')
+print (h1)
+print('RGB =', tuple(int(h1[i:i+2], 16) for i in (0, 2, 4)))
--- Guess the color.py
+++ Guess the color.py
@@ -13,62 +13,110 @@ color_4 = ["#" + ''.join([random.choice('0123456789ABCDEF') for j in range(6)])f
 color_5 = ["#" + ''.join([random.choice('0123456789ABCDEF') for j in range(6)])for i in range(1)]
 color_6 = ["#" + ''.join([random.choice('0123456789ABCDEF') for j in range(6)])for i in range(1)]
 
+# def generator_color(rgb):
+#     global t
+#     global arr
+#     global color
+#     rgb = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+#     lst = list(t)
+#     lst[0] = rgb[0]
+#     lst[1] = rgb[1]
+#     lst[2] = rgb[2]
+#     t = tuple(lst)
+#     arr.append(t)
+#     color = random.choice(arr)
+#     return "#%02x%02x%02x" % rgb
 
-def generator_color(rgb):
-    global t
-    global arr
-    global color
-    rgb = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-    lst = list(t)
-    lst[0] = rgb[0]
-    lst[1] = rgb[1]
-    lst[2] = rgb[2]
-    t = tuple(lst)
-    arr.append(t)
-    color = random.choice(arr)
-    return "#%02x%02x%02x" % rgb
-
-def switch(rgb):
-    print(rgb)
-
-
+def switch(bg):
+    hex_color = "".join(bg).lstrip('#')
+    rgb_color = tuple(int(hex_color[i:i + 2], 16) for i in (0, 2, 4))
+    if rgb_color == random_color:
+        print("YES")
+    else:
+        print("NO")
+    print(bg)
 
 
 def choice_1():
+    global arr
+    global random_color
     root = Tk.Tk()
     root.title("УГАДАЙ ЦВЕТ")
     root.geometry("1366x768")
     root.resizable(width=True, height=True)
     root.iconbitmap(default="forrst.ico")
-    btn1 = Tk.Button(root, width=45, height=10, bg=generator_color(rgb), text=root.cget('bg'), command=lambda: switch(color)).place(x=100, y=250)
-    btn2 = Tk.Button(root, width=45, height=10, bg=generator_color(rgb), text=root.cget('bg'), command=lambda: switch(color)).place(x=900, y=250)
-    label_1 = Tk.Label(root, text=f"УГАДАЙ ЭТОТ {color} ЦВЕТ", font='Arial 30').pack()
+    Tk.Button(root, width=45, height=10, bg=color_1, text=color_1, command=lambda: switch(bg=color_1)).place(x=100, y=250)
+    Tk.Button(root, width=45, height=10, bg=color_2, text=color_2, command=lambda: switch(bg=color_2)).place(x=900, y=250)
+    c1 = "".join(color_1).lstrip('#')
+    c2 = "".join(color_2).lstrip('#')
+    c1app = tuple(int(c1[i:i+2], 16) for i in (0, 2, 4))
+    c2app = tuple(int(c2[i:i+2], 16) for i in (0, 2, 4))
+    arr.append(c1app)
+    arr.append(c2app)
+    random_color = random.choice(arr)
+    Tk.Label(root, text=f"УГАДАЙ ЭТОТ {random_color} ЦВЕТ", font='Arial 30').pack()
 
 def choice_2():
+    global arr
+    global random_color
     root = Tk.Tk()
     root.title("УГАДАЙ ЦВЕТ")
     root.geometry("1366x768")
     root.resizable(width=True, height=True)
     root.iconbitmap(default="forrst.ico")
-    btn1 = Tk.Button(root, width=45, height=10, bg=generator_color(rgb)).place(x=100, y=250)
-    btn2 = Tk.Button(root, width=45, height=10, bg=generator_color(rgb)).place(x=900, y=250)
-    btn3 = Tk.Button(root, width=45, height=10, bg=generator_color(rgb)).place(x=100, y=450)
-    btn4 = Tk.Button(root, width=45, height=10, bg=generator_color(rgb)).place(x=900, y=450)
-    label_2 = Tk.Label(root, text=f"УГАДАЙ ЭТОТ {color} ЦВЕТ", font='Arial 30').pack()
+    Tk.Button(root, width=45, height=10, bg=color_1, text=color_1, command=lambda: switch(bg=color_1)).place(x=100, y=150)
+    Tk.Button(root, width=45, height=10, bg=color_2, text=color_2, command=lambda: switch(bg=color_2)).place(x=900, y=150)
+    Tk.Button(root, width=45, height=10, bg=color_3, text=color_3, command=lambda: switch(bg=color_3)).place(x=100, y=450)
+    Tk.Button(root, width=45, height=10, bg=color_4, text=color_4, command=lambda: switch(bg=color_4)).place(x=900, y=450)
+    c1 = "".join(color_1).lstrip('#')
+    c2 = "".join(color_2).lstrip('#')
+    c3 = "".join(color_1).lstrip('#')
+    c4 = "".join(color_2).lstrip('#')
+    c1app = tuple(int(c1[i:i+2], 16) for i in (0, 2, 4))
+    c2app = tuple(int(c2[i:i+2], 16) for i in (0, 2, 4))
+    c3app = tuple(int(c3[i:i+2], 16) for i in (0, 2, 4))
+    c4app = tuple(int(c4[i:i+2], 16) for i in (0, 2, 4))
+    arr.append(c1app)
+    arr.append(c2app)
+    arr.append(c3app)
+    arr.append(c4app)
+    random_color = random.choice(arr)
+    Tk.Label(root, text=f"УГАДАЙ ЭТОТ {random_color} ЦВЕТ", font='Arial 30').pack()
 
 def choice_3():
+    global arr
+    global random_color
     root = Tk.Tk()
     root.title("УГАДАЙ ЦВЕТ")
     root.geometry("1366x768")
     root.resizable(width=True, height=True)
     root.iconbitmap(default="forrst.ico")
-    btn1 = Tk.Button(root, width=45, height=10, bg=generator_color(rgb)).place(x=100, y=250)
-    btn2 = Tk.Button(root, width=45, height=10, bg=generator_color(rgb)).place(x=500, y=250)
-    btn3 = Tk.Button(root, width=45, height=10, bg=generator_color(rgb)).place(x=900, y=250)
-    btn4 = Tk.Button(root, width=45, height=10, bg=generator_color(rgb)).place(x=100, y=450)
-    btn5 = Tk.Button(root, width=45, height=10, bg=generator_color(rgb)).place(x=500, y=450)
-    btn6 = Tk.Button(root, width=45, height=10, bg=generator_color(rgb)).place(x=900, y=450)
-    label_3 = Tk.Label(root, text=f"УГАДАЙ ЭТОТ {color} ЦВЕТ", font='Arial 30').pack()
+    Tk.Button(root, width=45, height=10, bg=color_1, text=color_1, command=lambda: switch(bg=color_1)).place(x=100, y=250)
+    Tk.Button(root, width=45, height=10, bg=color_2, text=color_2, command=lambda: switch(bg=color_2)).place(x=900, y=250)
+    Tk.Button(root, width=45, height=10, bg=color_3, text=color_3, command=lambda: switch(bg=color_3)).place(x=100, y=550)
+    Tk.Button(root, width=45, height=10, bg=color_4, text=color_4, command=lambda: switch(bg=color_4)).place(x=900, y=550)
+    Tk.Button(root, width=45, height=10, bg=color_5, text=color_5, command=lambda: switch(bg=color_5)).place(x=100, y=950)
+    Tk.Button(root, width=45, height=10, bg=color_6, text=color_6, command=lambda: switch(bg=color_6)).place(x=900, y=950)
+    c1 = "".join(color_1).lstrip('#')
+    c2 = "".join(color_2).lstrip('#')
+    c3 = "".join(color_1).lstrip('#')
+    c4 = "".join(color_2).lstrip('#')
+    c5 = "".join(color_1).lstrip('#')
+    c6 = "".join(color_2).lstrip('#')
+    c1app = tuple(int(c1[i:i+2], 16) for i in (0, 2, 4))
+    c2app = tuple(int(c2[i:i+2], 16) for i in (0, 2, 4))
+    c3app = tuple(int(c3[i:i+2], 16) for i in (0, 2, 4))
+    c4app = tuple(int(c4[i:i+2], 16) for i in (0, 2, 4))
+    c5app = tuple(int(c5[i:i+2], 16) for i in (0, 2, 4))
+    c6app = tuple(int(c6[i:i+2], 16) for i in (0, 2, 4))
+    arr.append(c1app)
+    arr.append(c2app)
+    arr.append(c3app)
+    arr.append(c4app)
+    arr.append(c5app)
+    arr.append(c6app)
+    random_color = random.choice(arr)
+    Tk.Label(root, text=f"УГАДАЙ ЭТОТ {random_color} ЦВЕТ", font='Arial 30').pack()
 
 def guess():
     window = Tk.Tk()
